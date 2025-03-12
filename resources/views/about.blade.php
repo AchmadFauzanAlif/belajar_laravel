a37fdb84c82dcc19550cbba7afc8fb45a235c232
Achmad Fauzan Alif Fitrah <fznfitrah@gmail.com>
2025-03-12 21:37:32 +0700

    build: Repo Belajar Laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+</head>
+<body>
+    <h1>Welcome To About</h1>
+
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+
+    <h3>Nama : {{ $nama }}</h3>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis magnam sed error placeat asperiores aliquam eligendi dolore qui accusantium quis voluptate ducimus, omnis magni deleniti nemo ipsa. Quasi, voluptatibus et!</p>
+</body>
+</html>
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web Blog</title>
+</head>
+<body>
+    <h1>Web Tempat Baca Berita</h1>
+
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+
+    <article>
+        <h2>Maling Bensin</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam rem distinctio suscipit modi, perspiciatis vero quo quasi quis laborum illum, maiores dignissimos ducimus dolore libero laboriosam placeat, impedit nobis alias dolorum quaerat debitis eum ipsam error est? Perferendis blanditiis necessitatibus officia consequuntur veritatis qui facere officiis non asperiores, cum modi, eaque, beatae magni corrupti esse laboriosam et repellendus quas. Non dolor ad esse. Velit repudiandae ex, nam vel, consequuntur nostrum neque blanditiis ipsum optio quod dolorem voluptatum nobis sit in asperiores est harum quia voluptatibus ducimus at iure vero. Fugit optio iste, sit aliquid fuga quos unde non quasi quibusdam.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est totam dolores doloribus tempora dignissimos. Dolores nostrum omnis sunt laboriosam sed atque error eligendi ullam provident ducimus. Fugiat a assumenda quasi.</p>
+    </article>
+
+    <article>
+        <h2>Maling Kayu</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur laborum numquam odit dicta voluptatibus at omnis autem a quisquam ab quasi adipisci quam, libero explicabo saepe. Ad quasi aspernatur veritatis laudantium. Iste repudiandae quo, aliquid dolore quibusdam nemo modi impedit quod, fugit doloremque dolorum optio error amet voluptas, repellat nulla.</p>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae quisquam placeat amet, asperiores provident quos distinctio? Commodi non, sit totam neque atque omnis. Ipsam magnam nihil aliquam animi voluptas necessitatibus similique dolore et, mollitia totam architecto odio, veniam reiciendis in ipsum corrupti neque esse expedita. Ipsa alias id ex vel eos velit corrupti iusto. Saepe impedit atque tempora soluta perferendis?</p>
+    </article>
+
+
+</body>
+</html>
